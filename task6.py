d0ffc3da96d296c60598a968c355dd1650fcae48
nicola <74143605+nicolablom@users.noreply.github.com>
2020-12-03 17:58:23 +0100

    Update task6.py

--- task6.py
+++ task6.py
@@ -26,44 +26,26 @@ def instances(path, keyword):
     sentences_with_keyword = []
     result = []
 
-    # add spaces to keyword, so that words containing the keyword don't count
-    keyword = ' ' + keyword + ' '
-
     # make keyword lower, so that capitalization doesn't become an issue
     keyword = keyword.lower()
-
     # insert markers for alternative punctuation, so that origianal punctuation can be restored in the end
-    content = content.replace('!', 'ecsm.')
-    content = content.replace('?', 'qtsm.')
-    content = content.replace('--', 'slsm.')
-    content = content.strip() # taking out \n
-    content = content.replace('\n', ' ') # sometimes \n are undetected if there is no space before
-
+    punctuation = ['ecsm.', 'gtsm.', 'slsm.', 'ccma']
+    marks = ['!', '?', '--', ',']
+    for k, v in enumerate(marks):
+        content = content.replace(v, punctuation[k])
+    content = content.replace('\n', ' ')
     # convert text into list, which is split into single sentences
     list_content = content.split('.')
-
     # first append all sentences containing keyword into the specified list
     for i in list_content:
         if keyword in i.lower():
             sentences_with_keyword.append(i)
-
-    # restore original punctuation to the sentences and add them to a final list
     for i in sentences_with_keyword:
-        if 'ecsm' in i:
-            i = i.replace('ecsm', '!')
-            result.append(i)
-        elif 'gtsm' in i:
-            i = i.replace('gtsm', '?')
-            result.append(i)
-        elif 'slsm' in i:
-            i = i.replace('slsm', '--')
-            result.append(i)
-        elif i == '':
-            pass
-        else:
-            i = i + '.'
-            result.append(i)
-
+        i = i + '.'
+        for k, v in enumerate(punctuation):
+            if v in i:
+                i = i.replace(v, marks[k])
+        result.append(i)
     return result
 
 # testing
